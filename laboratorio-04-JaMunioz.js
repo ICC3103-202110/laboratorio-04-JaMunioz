10f486d957a118b11f0bc96cb941afd5f0b06ee1
JaMunioz <jamunoz6@miuandes.cl>
2021-05-06 17:01:16 -0400

    Third Commit

--- laboratorio-04-JaMunioz.js
+++ laboratorio-04-JaMunioz.js
@@ -5,23 +5,33 @@ function view(counter){
 }
 
 function update(msg, counter){
-    if (msg == "+"){
-        return counter ++;
-    }
-    if (msg == "-"){
-        return counter --;
+
+    if (msg == '+') {
+        counter += 1;
+        return counter;
+    }   
+    else{
+        if (msg == '-') {
+            counter -= 1;
+            return counter;
+        }   
+        else{
+            return counter;  
+        }
     }
 }
 
 //input
 let prompt = require('prompt-sync')();
 
+let counter = 0;
+let msg;
+
 while (true){
     process.stdout.write('\033c');
-    let counter = 0;
     console.log(view(counter));
-    let msg = prompt('What  would you do? ');
-    update(msg, counter);
+    msg = prompt('What  would you do? ');
+    counter = update(msg, counter);
     if (counter == 2){ 
         break;
     }
