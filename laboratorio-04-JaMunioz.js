1e123299c9efefb4607b2c17d8046204a808f7bf
JaMunioz <jamunoz6@miuandes.cl>
2021-05-06 16:44:42 -0400

    second commit

--- laboratorio-04-JaMunioz.js
+++ laboratorio-04-JaMunioz.js
@@ -1,5 +1,28 @@
+function view(counter){
+    return (`        Count: ${counter}\n                         \n`
+    +`        (+) (-)          \n                         \n`
+    +`                         \n        (q) for quit     \n`)
+}
+
+function update(msg, counter){
+    if (msg == "+"){
+        return counter ++;
+    }
+    if (msg == "-"){
+        return counter --;
+    }
+}
+
 //input
 let prompt = require('prompt-sync')();
- 
-let numero = prompt('Ingrese el numero que quiere: ');
-console.log(`El numero elegido es: ${numero}!`);
+
+while (true){
+    process.stdout.write('\033c');
+    let counter = 0;
+    console.log(view(counter));
+    let msg = prompt('What  would you do? ');
+    update(msg, counter);
+    if (counter == 2){ 
+        break;
+    }
+}
